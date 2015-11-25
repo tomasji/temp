998fb5b7209884a7cb4b4c5b42c83a47abc8b30b
Tomas Jirka <tomasj@salsitasoft.com>
2015-11-25 21:48:41 +0100

    database

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,6 +1,30 @@
 const express = require('express');
 const router = new express.Router();
+const pg = require('pg');
 module.exports = router;
+
+const connectionString = process.env.DATABASE_URL || 'postgres://usr:usr_password@test_db00:5432/test';
+
+//--------------------------------------------------------------------------------
+function getData(sql, params, callback) {
+    pg.connect(connectionString, (connectErr, client, done) => {
+        if (connectErr) {
+            /*eslint-disable no-console*/
+            return console.error('error fetching client from pool', connectErr);
+            /*eslint-enable no-console*/
+        }
+        client.query(sql, params, (err, result) => {
+            done();
+            if (err) {
+                /*eslint-disable no-console*/
+                console.error('Error running query: ', err, '\n' + sql, ' [', params, ']');
+                /*eslint-enable no-console*/
+                return callback('Error running query ' + sql + ' ' + err);
+            }
+            callback(result.rows);
+        });
+    });
+}
 
 function escapeHTML(s) {
     return s.replace(/&/g, '&amp;')
@@ -15,4 +39,7 @@ const names = Object.keys(env).sort();
 for (const v of names) {
     vars += escapeHTML(v) + '=' + escapeHTML(process.env[v]) + '<br/>';
 }
-router.get('/', (req, res) => { res.render('ww-test', {page: 'test_suite', vars: vars}); });
+
+getData('SELECT name FROM test ORDER BY name', [], (dbData) => {
+    router.get('/', (req, res) => { res.render('ww-test', {page: 'test_suite', vars: vars, db_data: dbData}); });
+});
