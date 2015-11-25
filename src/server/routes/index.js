943991e27caef1e9d8d2c6d22eff24a3a7a18a29
Tomas Jirka <tomasj@salsitasoft.com>
2015-11-25 20:18:28 +0100

    env vars

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,5 +2,17 @@ const express = require('express');
 const router = new express.Router();
 module.exports = router;
 
+function escapeHTML(s) {
+    return s.replace(/&/g, '&amp;')
+            .replace(/"/g, '&quot;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;');
+}
     // Main page
-router.get('/', (req, res) => { res.render('ww-test', {page: 'test_suite'}); });
+let vars = '';
+const env = process.env;
+const names = Object.keys(env).sort();
+for (const v of names) {
+    vars += escapeHTML(v) + '=' + escapeHTML(process.env[v]) + '<br/>';
+}
+router.get('/', (req, res) => { res.render('ww-test', {page: 'test_suite', vars: vars}); });
