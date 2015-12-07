e5251716b7635a6beb776c0741d62fe05bd4f7e6
Tomas Jirka <tomasj@salsitasoft.com>
2015-12-07 19:02:33 +0100

    shi

--- src/server/routes/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = new express.Router();
-module.exports = router;
-
-    // Main page
-router.get('/', (req, res) => { res.render('ww-test', {page: 'test_suite'}); });
