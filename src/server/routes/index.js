6b704ac1dcf3ba1f14bf6df12525aaa35569f064
Tomas Jirka <tomasj@salsitasoft.com>
2016-03-19 10:53:31 +0100

    pokus

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,17 +2,5 @@ const express = require('express');
 const router = new express.Router();
 module.exports = router;
 
-function escapeHTML(s) {
-    return s.replace(/&/g, '&amp;')
-            .replace(/"/g, '&quot;')
-            .replace(/</g, '&lt;')
-            .replace(/>/g, '&gt;');
-}
     // Main page
-let vars = '';
-const env = process.env;
-const names = Object.keys(env).sort();
-for (const v of names) {
-    vars += escapeHTML(v) + '=' + escapeHTML(process.env[v]) + '<br/>';
-}
-router.get('/', (req, res) => { res.render('ww-test', {page: 'test_suite', vars: vars}); });
+router.get('/', (req, res) => { res.render('ww-test', {data: process.env}); });
